3b426acc456f64d90ec73d6849eaf9f615680dae
Sini Jaakkola <sini.k.jaakkola@gmail.com>
2016-03-18 10:06:06 +0800

    created db and migration file

---
+++ db/migrate/20160318095000_create_contacts.rb
@@ -0,0 +1,15 @@
+require_relative '../application'
+
+class CreateContacts < ActiveRecord::Migration
+  def change
+ 
+    create_table :contacts do |t|
+    	t.string :name
+    	t.string :phone
+    	t.string :address
+    	t.timestamps
+
+    end
+  end
+end
+
